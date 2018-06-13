ef063c9e78988325709939cf243d4e039acd4e50
Warren Read <wread@ebi.ac.uk>
2018-06-13 13:49:04 +0100

    Fix parameter numbering in commented list; ignore csv files

--- ontomapper.py
+++ ontomapper.py
@@ -25,23 +25,23 @@ __PARAMETERS__ (to main program: all non-positional key/value pairs, to be initi
     3. layout        : layout of new entries in output spreadsheet: 'in-situ', 'uni-column', 'multi-column', 'uni-row'
                        or 'multi-row'; default = 'multi-column'
     4. file-format   : input / output spreadsheet file format: 'csv' or 'tsv'; default = 'tsv'
-    4. column-index  : column index (starting at 0) to parse for source IRIs; default based on header search?
-    5. keep          : whether to retain column containing source IRIs in output; boolean, default = no
-    6. target        : ontolog[y|ies] to search for equivalent terms; default = MeSH
-    7. uri-format    : return format of target IRIs (curies, long form, etc.); default = long form
-    8. boundary      : boundary value (%age) for confidence level; default = 100 (OxO distance 1)
-    9. oxo_url       : URL of OxO web service; default = public OxO server
-    10. paxo         : use 'experimental' PAXO code?; boolean, default = no
-    11. config       : configuration file path
-    12. quantity     : no. of query terms to include in single API call
-    13. verbose      : do we want a bunch of large json objects dumped to standard error?; boolean, default = no
-    14. ????         : source ontology from which terms in existing spreadsheet come; default = 'EFO' --- Probably
+    5. column-index  : column index (starting at 0) to parse for source IRIs; default based on header search?
+    6. keep          : whether to retain column containing source IRIs in output; boolean, default = no
+    7. target        : ontolog[y|ies] to search for equivalent terms; default = MeSH
+    8. uri-format    : return format of target IRIs (curies, long form, etc.); default = long form
+    9. boundary      : boundary value (%age) for confidence level; default = 100 (OxO distance 1)
+    10. oxo_url       : URL of OxO web service; default = public OxO server
+    11. paxo         : use 'experimental' PAXO code?; boolean, default = no
+    12. config       : configuration file path
+    13. quantity     : no. of query terms to include in single API call
+    14. verbose      : do we want a bunch of large json objects dumped to standard error?; boolean, default = no
+    15. ????         : source ontology from which terms in existing spreadsheet come; default = 'EFO' --- Probably
                        unnecessary! --- check!!
-    15. ????         : input format of source IRIs?
-    16. ????         : negation operators for the three boolean parameters? ... probably unnecessary.
-    17. ????         : column header as mutually exlusive alternative to column index? --- does this have implications
+    16. ????         : input format of source IRIs?
+    17. ????         : negation operators for the three boolean parameters? ... probably unnecessary.
+    18. ????         : column header as mutually exlusive alternative to column index? --- does this have implications
                        for processing of the column, inside and outside pandas?
-    18. ????         : option to change format of incoming IRIs before re-output (implies --keep)
+    19. ????         : option to change format of incoming IRIs before re-output (implies --keep)
 
 
 __FUNCTIONS__
